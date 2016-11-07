3ee4fd113c8489279898277e96bac4a19e45a2b8
Brandon Horst <brandonhorst@gmail.com>
2016-11-07 14:50:13 -0500

    Working commit

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@
 import _ from 'lodash'
 import { createElement } from 'elliptical'
 import { Command, String, Integer, Decimal, URL, PhoneNumber, EmailAddress, Date, Time, DateTime} from 'lacona-phrases'
-import { showNotification } from 'lacona-api'
+import { showNotification, runApplescript } from 'lacona-api'
 import { join } from 'path'
 import fetch from 'node-fetch'
 
@@ -27,7 +27,7 @@ const IFTTTCommand = {
   execute (result, {config}) {
     const event = encodeURIComponent(result.event)
     fetch(
-      `https://maker.ifttt.com/trigger/${event}/with/key/${config.ifttt.key}`, {
+      `https://maker.ifttt.com/trigger/${event}/with/key/${config.key}`, {
         method: 'POST',
         body: JSON.stringify({value1: result.argument}),
         headers: {'Content-Type': 'application/json'}
@@ -46,8 +46,8 @@ const IFTTTCommand = {
   },
 
   describe ({config}) {
-    if (config.ifttt.key) {
-      const triggers = _.chain(config.ifttt.triggers)
+    if (config.key) {
+      const triggers = _.chain(config.triggers)
         .filter('event')
         .map(({event, argument}) => {
           let ArgumentElement = argumentTypeMap[argument]
@@ -78,4 +78,58 @@ const IFTTTCommand = {
   }
 }
 
+const VALID_ARGUMENTS = {
+  none: '',
+  string: 'a String',
+  integer: 'an Integer',
+  decimal: 'a Decimal',
+  url: 'a URL',
+  email: 'an Email Address',
+  phone: 'a Phone Number',
+  time: 'a Time',
+  date: 'a Date',
+  datetime: 'a Date and Time'
+}
+
+async function onURLCommand (command, query, {config, setConfig}) {
+  if (command !== 'add-trigger') {
+    console.error('lacona-ifttt command not recognized. URL should be in the form https://lacona-ifttt/add-trigger?event=x')
+    return
+  }
+  if (!query.event) {
+    console.error('lacona-ifttt/add-trigger event was not provided. URL should be in the form https://lacona-ifttt/add-trigger?event=x')
+    return 
+  }
+  if (query.argument && !_.includes(_.keys(VALID_ARGUMENTS), query.argument)) {
+    console.error('lacona-ifttt/add-trigger invalid argument type')
+    return
+  }
+
+  if (_.find(config.triggers, trigger => trigger.event === query.event && trigger.argument === (query.argument || 'none'))) {
+    const text = `Lacona already has a trigger called ${query.event}. Check the IFTTT Addon preferences.`
+    const textEscaped = text.replace(/"/g, '\\"')
+    await runApplescript({script: `display dialog "${textEscaped}" buttons {"OK"}`})
+    return
+  }
+
+  // if we made it this far
+  const argumentText = (query.argument && query.argument !== 'none')
+    ? ` with ${VALID_ARGUMENTS[query.argument]} argument?`
+    : '?'
+  const newEntry = `Would you like to add a Lacona IFTTT trigger called "${query.event}"${argumentText}`
+  const newEntryEscaped = newEntry.replace(/"/g, '\\"')
+
+  const response = await runApplescript({script: `
+    set question to display dialog "${newEntryEscaped}" buttons {"Yes", "No"} default button 2
+    return button returned of question
+  `})
+  if (response === 'Yes') {
+    const newConfig = _.cloneDeep(config)
+    newConfig.triggers.push({event: query.event, argument: query.argument || 'none'})
+  console.log("RES", newConfig)
+    setConfig(newConfig)
+  }
+}
+
 export const extensions = [IFTTTCommand]
+export const hooks = {onURLCommand}
